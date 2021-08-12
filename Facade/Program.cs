7592e17369ac68f674db01754ebabf782966dfcc
Mehmet D <exortek979@gmail.com>
2021-08-12 13:52:56 +0300

    -

--- Facade/Program.cs
+++ Facade/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Facade
 {
@@ -10,6 +6,81 @@ namespace Facade
     {
         static void Main(string[] args)
         {
+            CustomerManager customer = new CustomerManager();
+            customer.Save();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Logging : ILogging
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged");
+        }
+    }
+
+    internal interface ILogging
+    {
+        void Log();
+    }
+
+    class Caching : ICaching
+    {
+        public void Cache()
+        {
+            Console.WriteLine("Cached");
+        }
+    }
+
+    internal interface ICaching
+    {
+        void Cache();
+    }
+
+    class Authorize : IAuthorize
+    {
+        public void CheckUser()
+        {
+            Console.WriteLine("Checked");
+        }
+    }
+
+    internal interface IAuthorize
+    {
+        void CheckUser();
+    }
+
+    class CustomerManager
+    {
+        private CrossCuttingConcernsFacade _concerns;
+
+        public CustomerManager()
+        {
+            _concerns = new CrossCuttingConcernsFacade();
+        }
+
+        public void Save()
+        {
+            _concerns.Logging.Log();
+            _concerns.Authorize.CheckUser();
+            _concerns.Caching.Cache();
+            Console.WriteLine("Saved");
+        }
+    }
+
+    class CrossCuttingConcernsFacade
+    {
+        public ILogging Logging;
+        public ICaching Caching;
+        public IAuthorize Authorize;
+
+        public CrossCuttingConcernsFacade()
+        {
+            Logging = new Logging();
+            Caching = new Caching();
+            Authorize = new Authorize();
         }
     }
 }
