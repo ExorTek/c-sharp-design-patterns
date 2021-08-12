c518bd3ac7ebb7d60f3a3cea6bef34d3aaa97eab
Mehmet D <exortek979@gmail.com>
2021-08-12 14:14:09 +0300

    -

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Adapter
 {
@@ -10,6 +6,63 @@ namespace Adapter
     {
         static void Main(string[] args)
         {
+            ProductManager productManager = new ProductManager(new MdLogger());
+            productManager.Save();
+            Console.WriteLine("-----------------");
+            ProductManager productManager1 = new ProductManager(new Log4NetAdapter());
+            productManager1.Save();
+            Console.ReadKey();
+        }
+    }
+
+    class ProductManager : IProductService
+    {
+        private ILogger _logger;
+
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log("UserData");
+            Console.WriteLine("Saved");
+        }
+    }
+
+    interface IProductService
+    {
+        void Save();
+    }
+
+    interface ILogger
+    {
+        void Log(string message);
+    }
+
+    class MdLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"Logged with mdlogger, {message}");
+        }
+    }
+
+    class Log4Net
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine($"Logged with log4net, {message}");
+        }
+    }
+
+    class Log4NetAdapter : ILogger
+    {
+        public void Log(string message)
+        {
+            Log4Net log4Net = new Log4Net();
+            log4Net.LogMessage(message);
         }
     }
 }
