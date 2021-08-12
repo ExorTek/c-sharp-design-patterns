42d166adb95205b5535b6683084f218353d45b84
Mehmet D <exortek979@gmail.com>
2021-08-12 13:39:26 +0300

    -

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -10,6 +10,92 @@ namespace Builder
     {
         static void Main(string[] args)
         {
+            ProductDirector productDirector = new ProductDirector();
+            var builder = new NewCustomerProductBuilder();
+            productDirector.GenerateProduct(builder);
+            var model = builder.GetModel();
+            Console.WriteLine("Category: "+model.CategoryName + "\n-----");
+            Console.WriteLine("ProductName: " + model.ProductName + "\n-----");
+            Console.WriteLine("Discount: " + model.Discount + "\n-----");
+            Console.WriteLine("DiscountApplied: " + model.DiscountApplied + "\n-----");
+            Console.WriteLine("Id: " + model.Id + "\n-----");
+            Console.WriteLine("UnitPrice: " + model.UnitPrice + "\n-----");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Discount { get; set; }
+        public bool DiscountApplied { get; set; }
+    }
+
+    public abstract class ProductBuilder
+    {
+        public abstract void GetAll();
+        public abstract void ApplyDiscount();
+        public abstract ProductViewModel GetModel();
+    }
+
+    public class NewCustomerProductBuilder : ProductBuilder
+    {
+        private ProductViewModel model = new ProductViewModel();
+
+        public override void GetAll()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverages";
+            model.ProductName = "Chai";
+            model.UnitPrice = 20;
+        }
+
+        public override void ApplyDiscount()
+        {
+            model.Discount = 17;
+            model.DiscountApplied = true;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+    }
+
+    public class OldCustomerProductBuilder : ProductBuilder
+    {
+        private ProductViewModel model = new ProductViewModel();
+
+        public override void GetAll()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverages";
+            model.ProductName = "Chai";
+            model.UnitPrice = 20;
+        }
+
+        public override void ApplyDiscount()
+        {
+            model.Discount = 17;
+            model.DiscountApplied = false;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+    }
+
+    public class ProductDirector
+    {
+        public void GenerateProduct(ProductBuilder productBuilder)
+        {
+            productBuilder.GetAll();
+            productBuilder.ApplyDiscount();
         }
     }
 }
