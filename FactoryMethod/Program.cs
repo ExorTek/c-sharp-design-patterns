6d6bea5bc82f23fc2db6cfd74efc353875bc7799
Mehmet D <exortek979@gmail.com>
2021-08-12 11:35:12 +0300

    -

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Save();
+
+            Console.ReadKey();
+        }
+    }
+
+    class LoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new MdLogger();
+        }
+    }
+
+    internal interface ILoggerFactory
+    {
+    }
+
+    internal interface ILogger
+    {
+        void Log();
+    }
+
+    class MdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged Md");
+
+        }
+    }
+
+    class CustomerManager
+    {
+       public void Save()
+        {
+            Console.WriteLine("Saved!!");
+            ILogger logger = new LoggerFactory().CreateLogger();
+            logger.Log();
+        }
+    }
+}
---
+++ Singeleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singeleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = CustomerManager.CreateAsSingleton();
+            customer.Save();
+            Console.ReadKey();
+        }
+    }
+
+    class CustomerManager
+    {
+        private static CustomerManager _customerManager;
+
+        private CustomerManager()
+        {
+        }
+
+        public static CustomerManager CreateAsSingleton()
+        {
+            return _customerManager ?? (_customerManager = new CustomerManager());
+        }
+
+        public  void Save()
+        {
+            Console.WriteLine("Saved!!");
+        }
+    }
+}
